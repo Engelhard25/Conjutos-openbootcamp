e911f4a7d56be6b9b4063e3142c514756c3a99c9
engelhard <engelhard25@hotmail.com>
2022-08-26 14:41:25 -0400

    Conjuntos.js

---
+++ Conjuntos.js
@@ -0,0 +1,12 @@
+// Conjuntos.js
+
+const Familia = ["Engelhard", "Eduard", "Zenaida", "Ana", "Tony", "Darving"];
+const Familia_Set = new Set(Familia);
+
+console.log(Familia_Set);
+
+Familia_Set.add("Engelhard");
+console.log(Familia_Set);
+
+Familia_Set.add("Javascript");
+console.log(Familia_Set);
